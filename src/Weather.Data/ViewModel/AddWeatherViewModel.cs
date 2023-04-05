3b8a3121c8edd20602423994ab8c0bf6fa90230c
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-05 20:27:21 -0300

    feat: add new ViewModel to represent data to add new Weather

---
+++ src/Weather.Data/ViewModel/AddWeatherViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Weather.Data.Models;
+
+namespace Weather.Data.ViewModel
+{
+    public class AddWeatherViewModel
+    {
+        public float Temperature { get; set; }
+        public TypeWeather Type { get; set; }
+    }
+}
