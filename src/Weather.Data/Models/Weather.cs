b82212499706a3180f124e504429cf84d4f119ca
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-05 16:18:38 -0300

    feat: add Weather class

---
+++ src/Weather.Data/Models/Weather.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather.Data.Models
+{
+    public class Weather
+    {
+        public float Temperature { get; set; }
+        public TypeWeather Type { get; set; }
+    }
+}   
