e9b5915768c4617ee4082d1183c0b811730303a3
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-05 16:30:38 -0300

    test: add unit test for WeatherController

---
+++ src/Weather.Test/ControllerTest/WeatherController_Test.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Weather.Test.ControllerTest
+{
+    [TestClass]
+    public class WeatherController_Test
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
