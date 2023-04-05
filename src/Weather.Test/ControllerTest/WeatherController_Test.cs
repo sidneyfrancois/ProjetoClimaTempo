7083910d5a3eb3b7626ec12772236eeb3229baca
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-05 18:36:09 -0300

    test: add test to compare the type of the returned model

--- src/Weather.Test/ControllerTest/WeatherController_Test.cs
+++ src/Weather.Test/ControllerTest/WeatherController_Test.cs
@@ -4,7 +4,6 @@ using System.Collections.Generic;
 using System.Web.Mvc;
 using Weather.Data.Models;
 using Weather.MVC.Controllers;
-using System.Linq;
 
 namespace Weather.Test.ControllerTest
 {
@@ -12,13 +11,11 @@ namespace Weather.Test.ControllerTest
     public class WeatherController_Test
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestReturnType()
         {
-            var listComparasion = new List<SingleWeather>();
-
             var controller = new WeatherController();
-            var result = controller.GetAllWeathers();
-            Assert.AreEqual(listComparasion, result);
+            var result = controller.GetAllWeathers() as ViewResult;
+            Assert.AreEqual(new List<SingleWeather>().GetType(), result.Model.GetType());
         }
     }
 }
