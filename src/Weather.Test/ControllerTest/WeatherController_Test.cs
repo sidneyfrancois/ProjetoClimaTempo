25a6dceb2d0634c3c833f5ee095aafc7aaa338c5
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-05 20:35:37 -0300

    test: check if the added object is of the same type

--- src/Weather.Test/ControllerTest/WeatherController_Test.cs
+++ src/Weather.Test/ControllerTest/WeatherController_Test.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Web.Mvc;
 using Weather.Data.Models;
+using Weather.Data.ViewModel;
 using Weather.MVC.Controllers;
 
 namespace Weather.Test.ControllerTest
@@ -16,6 +17,21 @@ namespace Weather.Test.ControllerTest
             var controller = new WeatherController();
             var result = controller.GetAllWeathers() as ViewResult;
             Assert.AreEqual(new List<SingleWeather>().GetType(), result.Model.GetType());
+        }
+
+        [TestMethod]
+        public void TestAddWeather()
+        {
+            var viewModel = new AddWeatherViewModel()
+            {
+                Temperature = 25,
+                Type = TypeWeather.Ensolarado
+            };
+
+            var controller = new WeatherController();
+            var result = controller.AddWeather(viewModel) as ViewResult;
+
+            Assert.AreEqual(new SingleWeather().GetType(), result.Model.GetType());
         }
     }
 }
