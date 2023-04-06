8c36f41b8eafb1a2feda4ec9f683507816aae9fe
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-06 11:08:51 -0300

    test: verify if return type of controller action is correct

---
+++ src/Weather.Test/ControllerTest/PrevisaoClimaTempoController_Test.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Weather.Data.Models;
+using Weather.MVC.Controllers;
+
+namespace Weather.Test.ControllerTest
+{
+    [TestClass]
+    public class PrevisaoClimaTempoController_Test
+    {
+        [TestMethod]
+        public void GetAllCidades_Test()
+        {
+            var controller = new PrevisaoClimaTempoController();
+            var result = controller.GetAllCidades() as ViewResult;
+            Assert.AreEqual(new List<Cidade>().GetType(), result.Model.GetType());
+        }
+    }
+}
