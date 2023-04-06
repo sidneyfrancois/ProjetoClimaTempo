d2de5debf16d2be9d6be42cdba95d6e43dbbb232
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-06 11:08:26 -0300

    feat: add PrevisaoClimaTempoController

---
+++ src/Weather.MVC/Controllers/PrevisaoClimaTempoController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Weather.Data.Context;
+
+namespace Weather.MVC.Controllers
+{
+    [RoutePrefix("clima")]
+    public class PrevisaoClimaTempoController : Controller
+    {
+        private readonly MyDbContext _context;
+
+        public PrevisaoClimaTempoController()
+        {
+            _context = new MyDbContext();
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("cidades")]
+        public ActionResult GetAllCidades()
+        {
+            var result = _context.Cidades.ToList();
+            return View(result);
+        }
+    }
+}
