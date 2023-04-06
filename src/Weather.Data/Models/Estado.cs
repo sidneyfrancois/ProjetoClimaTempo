8884d1188819a46921fb10fdb613c9bbcb31c0f2
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-06 10:31:34 -0300

    feat: add Estado class

---
+++ src/Weather.Data/Models/Estado.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather.Data.Models
+{
+    [Table("Estado")]
+    public class Estado
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("Nome")]
+        [MaxLength(200)]
+        public string Nome { get; set; }
+
+        [Required]
+        [Column("UF")]
+        [MaxLength(2)]
+        public string UF { get; set; }
+    }
+}
