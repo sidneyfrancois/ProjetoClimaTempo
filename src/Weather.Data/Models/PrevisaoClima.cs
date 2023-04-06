a8bc3ebcdaa92ecf736bde5fdbd54af70dc143a1
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-06 10:36:32 -0300

    feat: add PrevisaoClima class

---
+++ src/Weather.Data/Models/PrevisaoClima.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather.Data.Models
+{
+    [Table("PrevisaoClima")]
+    public class PrevisaoClima
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("CidadeId")]
+        public int CidadeId { get; set; }
+        public Cidade Cidade { get; set; }
+
+        [Required]
+        [Column("DataPrevisao")]
+        public DateTime DataPrevisao { get; set; }
+
+        [Required]
+        [Column("Clima")]
+        [MaxLength(15)]
+        public string Clima { get; set; }
+
+        [Required]
+        [Column("TemperaturaMinima")]
+        public float TemperaturaMinima { get; set; }
+
+        [Required]
+        [Column("TemperaturaMaxima")]
+        public float TemperaturaMaxima { get; set; }
+    }
+}
