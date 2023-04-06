7b9f5e0fc08ffe4e046cb672af7daa314996889b
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-06 10:26:52 -0300

    feat: add Cidade model

---
+++ src/Weather.Data/Models/Cidade.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather.Data.Models
+{
+    [Table("Cidade")]
+    public class Cidade
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("Nome")]
+        [MaxLength(200)]
+        public string Nome { get; set; }
+
+        [Required]
+        [Column("EstadoId")]
+        public int EstadoId { get; set; }
+        
+    }
+}
