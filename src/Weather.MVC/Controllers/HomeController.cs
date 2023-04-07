c28bc9845a03768731ab9193cef0889a57fa7011
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-07 11:07:49 -0300

    feat: add context into Home controller

--- src/Weather.MVC/Controllers/HomeController.cs
+++ src/Weather.MVC/Controllers/HomeController.cs
@@ -3,11 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Weather.Data.Context;
 
 namespace Weather.MVC.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly MyDbContext _context;
+
+        public HomeController()
+        {
+            _context = new MyDbContext();
+        }
+
         public ActionResult Index()
         {
             return View();
