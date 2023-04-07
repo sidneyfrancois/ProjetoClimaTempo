72e91978497d3eedac07a02ce76745a8ba839ca8
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-07 20:43:52 -0300

    feat: add new view model to list the wather in the next 7 days

---
+++ src/Weather.Data/ViewModel/WeatherReportViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather.Data.ViewModel
+{
+    class WeatherReportViewModel
+    {
+        public string Clima { get; set; }
+        public decimal TemperaturaMinima { get; set; }
+        public decimal TemperaturaMaxima { get; set; }
+    }
+}
