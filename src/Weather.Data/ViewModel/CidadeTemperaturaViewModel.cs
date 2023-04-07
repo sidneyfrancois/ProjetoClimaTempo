d486a8b34bbfc4036653f2d7016b1bf34610e940
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-07 16:56:24 -0300

    feat: add more simple ViewModel to avoid conflict

---
+++ src/Weather.Data/ViewModel/CidadeTemperaturaViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Weather.Data.ViewModel
+{
+    public class CidadeTemperaturaViewModel
+    {
+        public string NomeCidade { get; set; }
+        public decimal TemperatureMaxima { get; set; }
+        public decimal TemperatureMinima { get; set; }
+    }
+}
