6a8570a0db9902269db4ab2375b90f31f0ed3313
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-10 02:10:16 -0300

    feat: add Repository PrevisaClima, use delegates to customize top 3 temperatures function

---
+++ src/Weather.MVC/Repository/PrevisaoClimaRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Weather.Data.Context;
+using Weather.Data.Models;
+using System.Data.Entity;
+using System.Linq.Expressions;
+
+namespace Weather.MVC.Repository
+{
+    public class PrevisaoClimaRepository
+    {
+        private readonly MyDbContext _context;
+        public PrevisaoClimaRepository()
+        {
+            _context = new MyDbContext();
+        }
+
+        public List<PrevisaoClima> GetAll()
+        {
+            var allPrevisaoClima = _context.PrevisoesDeClima.Include(x => x.Cidade).ToList();
+            return allPrevisaoClima;
+        }
+
+        public List<PrevisaoClima> GetPrevisaoClimaTop3(
+            Expression<Func<PrevisaoClima, bool>> expressionWhere, 
+            Expression<Func<PrevisaoClima, object>> orderTempMaxOrMin, 
+            int minTake
+        )
+        {
+            var topTemperatures = _context.PrevisoesDeClima
+                                        .Include(x => x.Cidade)
+                                        .Where(expressionWhere)
+                                        .OrderBy(orderTempMaxOrMin)
+                                        .Take(minTake).ToList();
+            return topTemperatures;
+        }
+    }
+}
