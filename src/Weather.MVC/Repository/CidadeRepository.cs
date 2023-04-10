60594f68e3c2d90e86ccd4bbd1694a3c8b5a81bf
Sidney François <sidney.fvm.dev@outlook.com>
2023-04-10 01:50:23 -0300

    feat: add Cidade repository

---
+++ src/Weather.MVC/Repository/CidadeRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Weather.Data.Context;
+using Weather.Data.Models;
+
+namespace Weather.MVC.Services
+{
+    public class CidadeRepository
+    {
+        private readonly MyDbContext _context;
+        public CidadeRepository()
+        {
+            _context = new MyDbContext();
+        }
+
+        public SelectList GetAllCitiesForDropDownList()
+        {
+            var allCities = _context.Cidades.Select(c => new SelectListItem
+            {
+                Value = c.Id.ToString(),
+                Text = c.Nome
+            });
+
+            return new SelectList(allCities, "Value", "Text");
+        }
+    }
+}
